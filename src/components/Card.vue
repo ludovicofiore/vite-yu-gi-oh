b9c390cd9f79c80f9704a32476c914647130bab6
ludoff <ludovicofiore25@gmail.com>
2024-07-09 16:38:42 +0200

    feat: add card component and static layout of it

---
+++ src/components/Card.vue
@@ -0,0 +1,49 @@
+<script>
+
+export default {
+    name: 'Card',
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="image">
+
+        </div>
+
+        <div class="name">
+            <h3>NOME CARTA</h3>
+        </div>
+
+        <div class="archetype">
+            <span>archetipo</span>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as*;
+
+.card {
+    background-color: $primary;
+    flex-basis: calc(100% / 5 - 20px);
+    text-align: center;
+
+    .image {
+        width: 100%;
+        height: 200px;
+        background-color: aquamarine;
+    }
+
+    .name {
+        color: $secondary;
+        padding: 20px 0;
+    }
+
+    .archetype {
+        color: $tertiary;
+        padding-bottom: 15px;
+    }
+}
+</style>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,7 +1,11 @@
 <script>
+import Card from './Card.vue';
 
 export default {
     name: 'CardsList',
+    components: {
+        Card,
+    },
 }
 
 </script>
@@ -10,6 +14,11 @@ export default {
     <section class="container">
         <div class="found-cards">
             <span>Found 39 cards</span>
+        </div>
+
+
+        <div class="cards-container">
+            <Card />
         </div>
     </section>
 
@@ -28,6 +37,13 @@ export default {
         font-weight: bold;
         padding: 25px 15px;
 
+    }
+
+    .cards-container {
+
+        display: flex;
+        gap: 25px;
+        flex-wrap: wrap;
     }
 }
 </style>
