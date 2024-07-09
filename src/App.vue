b982e87e53ab09aa5be336876cd098c16057fb2f
ludoff <ludovicofiore25@gmail.com>
2024-07-09 16:19:51 +0200

    feat: add component for central content

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import AppHeader from './components/AppHeader.vue';
-
+import CardsList from "./components/CardsList.vue";
 
 export default {
   name: 'App',
   components: {
     AppHeader,
+    CardsList,
   }
 }
 </script>
@@ -14,9 +15,18 @@ export default {
 <template>
   <AppHeader />
 
+  <main>
+    <CardsList />
+  </main>
+
 </template>
 
 <style lang="scss">
 @use './style/general.scss' as *;
 @use './style/partials/variables' as*;
+
+main {
+  background-color: $primary;
+  padding: 50px 0;
+}
 </style>
---
+++ src/components/CardsList.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+    name: 'CardsList',
+}
+
+</script>
+
+<template>
+    <section class="container">
+        <div class="found-cards">
+            <span>Found 39 cards</span>
+        </div>
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as*;
+
+.container {
+    background-color: $secondary;
+    padding: 50px;
+
+    .found-cards {
+        background-color: $tertiary;
+        color: $secondary;
+        font-weight: bold;
+        padding: 25px 15px;
+
+    }
+}
+</style>
